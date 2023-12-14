e4d4a8b2b5b20b244feda7904c7196b20d357024
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 17:46:48 +0300

    Create D - XOR-конструктив2.cpp

---
+++ D - XOR-конструктив2.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int nex[200005][2];
+int exist[200005];
+int cnt = 0;
+int n;
+void ins(int x){
+    int p = 0;
+    for(int i = 20;i>=0;i--){
+        int t = (x >> i) & 1;
+        if(!nex[p][t]){
+            nex[p][t] = ++cnt;
+        }
+        p = nex[p][t];
+    }
+    exist[p] = true;
+    return ;
+}
+bool func(int x){
+    int ans = 0;
+    int p = 0;
+    for(int i = 20;i>=0;i--){
+        int t = (x >> i) & 1;
+        if(nex[p][t ^ 1]){
+            ans += (1 << i);
+            p = nex[p][t ^ 1];
+        }else p = nex[p][t];
+    }
+    return ans < n;
+}
+int main(){
+    cin>>n;
+    vector<int> vec(n+5);
+    for(int i =1;i<n;i++){
+        cin>>vec[i];
+        vec[i] ^= vec[i-1];
+        ins(vec[i]);
+    }
+    for(int i = 0;i<n;i++){
+        if(func(i)) {
+            for(int j = 0;j<n;j++){
+                cout<<(vec[j] ^ i)<<" ";
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
