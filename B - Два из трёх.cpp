4b4a55a2bc534eb9670234445b3a759d7eb8a6e7
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 17:45:05 +0300

    Create B - Два из трёх.cpp

---
+++ B - Два из трёх.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define endl '\n'
+#define VI vector<int>
+#define pii pair<int, int>
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+#define per(i, j, k) for (int i = j; i >= k; --i)
+#define print(a) cout << a << endl;
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve(int cs) {
+    int n;
+    cin >> n;
+    VI a(n), b(n, 1);
+    map<int, int> mp;
+    int cnt = 0;
+    rep(i, 0, n - 1) {
+        cin >> a[i];
+        if (mp[a[i]] == 1) {
+            cnt++;
+            if (cnt == 2) {
+                b[i] = 3;
+            }
+        }
+        else b[i] = 2;
+        mp[a[i]]++;
+    }
+    if (mp.size() == 1 || cnt < 2) {
+        cout << "-1\n";
+        return;
+    }
+    rep(i, 0, n - 1) cout << b[i] << " ";cout << endl;
+
+
+}   
+
+signed main() {
+    IO;
+    int T, TT = 1;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve(TT++);
+    }
+    return 0;
+}
