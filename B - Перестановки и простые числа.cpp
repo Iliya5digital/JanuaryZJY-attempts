69635486a2d7cdcf5b00d0755b6c8bb675a66feb
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 19:03:59 +0300

    Create B - Перестановки и простые числа.cpp

---
+++ B - Перестановки и простые числа.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve() {
+    int n;
+    cin >> n;
+    VI ans(n + 1);
+    ans[1] = 2, ans[n] = 3, ans[n / 2 + 1] = 1;
+    for (int i = 2, k = 4; i < n; i++) {
+        if (!ans[i]) {
+            ans[i] = k;
+            k++;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
+ 
