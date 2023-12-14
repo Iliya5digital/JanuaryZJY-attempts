cc42372b2c69f49886cbdecce1b39d88b8402cd7
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 18:58:34 +0300

    Create B - Юбилейные монеты.cpp

---
+++ B - Юбилейные монеты.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve() {
+    int m, k, w1, wk;
+    cin >> m >> k >> w1 >> wk;
+    int cw1 = max(m % k - w1, 0LL);
+    int cwk = max(m / k - wk, 0LL);
+    
+    if (w1 > m % k) {
+        int t = min(cwk, (w1 - m % k) / k);
+        cwk = cwk - t;
+    }
+
+    cout << cw1 + cwk << endl;
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
