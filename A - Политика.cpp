9c44f58dca78101d0c1ab5b843afce7d02826f2a
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 20:09:20 +0300

    Create A - Политика.cpp

---
+++ A - Политика.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+//#define LOCAL
+#ifdef LOCAL
+#include "LocalDebug.h"
+#endif
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    int sum = 1;
+    for (int i = 0; i < n - 1; i++) {
+        string s1;
+        cin >> s1;
+        if (s1 == s) {
+            sum++;
+        }
+    }
+
+    cout << sum << endl;
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
