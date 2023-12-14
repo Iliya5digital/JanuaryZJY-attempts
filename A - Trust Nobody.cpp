ecb1f87acff120752b7acda47f596e0456f38b8b
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 20:06:55 +0300

    Create A - Trust Nobody.cpp

---
+++ A - Trust Nobody.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+//#define LOCAL
+#ifdef LOCAL
+#include "LocalDebug.h"
+#endif
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i <= n; i++) {
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[j] > i) {
+                ans =  n - j;
+                break;
+            }
+        }
+
+        if (ans == i) {
+            cout << ans << endl;
+            return;
+        }
+    }
+
+    cout << "-1\n";
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
