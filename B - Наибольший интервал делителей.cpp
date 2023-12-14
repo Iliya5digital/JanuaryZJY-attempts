58fed81e3b54a8d3881842345c7f4fd0966c61ea
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 18:56:29 +0300

    Create B - Наибольший интервал делителей.cpp

---
+++ B - Наибольший интервал делителей.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int cnt = 0;
+    for (int i = 1; i <= 100; i++) {
+        if (n % i == 0) {
+            cnt++;
+        }
+        else {
+            ans = max(cnt, ans);
+            cnt = 0;
+        }
+    }
+    
+    cout << ans << endl;
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
