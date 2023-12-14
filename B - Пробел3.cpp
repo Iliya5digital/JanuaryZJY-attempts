30b21e6aabc30862deb5fd2dbd7026998ece392d
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 18:11:49 +0300

    Create B - Пробел3.cpp

---
+++ B - Пробел3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int T;
+	cin >> T;
+	while(T--) {
+        int n;
+        cin >> n;
+        int ans = 0, cnt = 0;
+        for (int i = 0, x; i < n; i++) {
+            cin >> x;
+            if (x == 0) cnt++;
+            else ans = max(ans, cnt);
+        }
+        cout << ans << endl;
+	}
+}
