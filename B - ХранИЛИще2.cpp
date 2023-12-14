6c498747eabe9e4fad4019e5e050c7f3c9fbbadd
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 17:38:53 +0300

    Create B - ХранИЛИще2.cpp

---
+++ B - ХранИЛИще2.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define endl '\n'
+#define VI vector<int>
+#define pii pair<int, int>
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+#define per(i, j, k) for (int i = j; i >= k; --i)
+#define print(a) cout << a << endl;
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+#define otto auto
+
+void solve(int cs) {
+    int n;
+    cin >> n;
+    VI a(n + 1, -1);
+    vector<VI > mp(n + 1, VI(n + 1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> mp[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) continue;
+            else if (a[i] == -1) a[i] = mp[i][j];
+            else a[i] &= mp[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i != j && mp[i][j] != (a[i] | a[j])) {
+                cout << "NO\n";
+                return;
+            }
+        }
+    }
+    cout << "YES\n";
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}   
+
+signed main() {
+    IO;
+    int T, TT = 1;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve(TT++);
+    }
+    return 0;
+}
