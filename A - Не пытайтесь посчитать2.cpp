8887d3d077ba801399003f1f0487e083057e5590
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 18:23:02 +0300

    Create A - Не пытайтесь посчитать2.cpp

---
+++ A - Не пытайтесь посчитать2.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+#define loop for (int i = 0; i < n; ++i)
+#define print(a) cout << a << endl;
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve(int cs) {
+    int n, m;
+    string x, s;
+    cin >> n >> m >> x >> s;
+    int ans = 0;
+    for (int i = 0; i < 10; i++) {
+        if (x.find(s) != -1) {
+            cout << i << endl;
+            return;
+        }
+        x += x;
+    }
+    cout << "-1\n";
+}   
+
+signed main() {
+    IO;
+    int T, TT = 1;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve(TT++);
+    }
+    return 0;
+}
