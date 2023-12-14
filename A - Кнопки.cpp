53ecd335b53fc5d75954382fd3d4fb296af80e61
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 19:00:00 +0300

    Create A - Кнопки.cpp

---
+++ A - Кнопки.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c & 1) {
+        a >= b ? cout << "First\n" : cout << "Second\n";
+        return;
+    }
+    else {
+        b >= a ? cout << "Second\n" : cout << "First\n";
+        return;
+    }
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
