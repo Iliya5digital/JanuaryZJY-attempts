71c2e520cec2c66b4d1e1032b91d35cd591ddddd
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 20:06:34 +0300

    Create A - LuoTianyi и строка-палиндром.cpp

---
+++ A - LuoTianyi и строка-палиндром.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+//#define LOCAL
+#ifdef LOCAL
+#include "LocalDebug.h"
+#endif
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+void solve() {
+    string s;
+    cin >> s;
+    for (int i = 1; i < s.size(); i++) {
+        int l = i, r = s.size() - 1;
+        while (l <= r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            }
+            else {
+                cout << s.size() - i << endl;
+                return;
+            }
+        }
+    }
+
+    cout << "-1\n";
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
