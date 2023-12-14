9e9feaa97ea359520271719c11ce0a28771ffe88
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 17:43:06 +0300

    Create B - Лаура и операции.cpp

---
+++ B - Лаура и операции.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define endl '\n'
+#define VI vector<int>
+#define pii pair<int, int>
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+#define per(i, j, k) for (int i = j; i >= k; --i)
+#define print(a) cout << a << endl;
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve(int cs) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a & 1) == (b & 1) && (b & 1) == (c & 1)) {
+        cout << "1 1 1\n";
+    }
+    else if ((a & 1) == (b & 1)) {
+        cout << "0 0 1\n";
+    }
+    else if ((a & 1) == (c & 1)) {
+        cout << "0 1 0\n";
+    }
+    else if ((c & 1) == (b & 1)) {
+        cout << "1 0 0\n";
+    }
+}   
+
+signed main() {
+    IO;
+    int T, TT = 1;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve(TT++);
+    }
+    return 0;
+}
