e0b4f48a3c392f04bd0e8a00387c50bc5f45d8ad
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 17:34:52 +0300

    Create B - Зарабатывание баллов2.cpp

---
+++ B - Зарабатывание баллов2.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define endl '\n'
+#define VI vector<int>
+#define pii pair<int, int>
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+#define per(i, j, k) for (int i = j; i >= k; --i)
+#define print(a) cout << a << endl;
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+#define otto auto
+
+void solve(int cs) {
+    int n, p, l, t;
+    cin >> n >> p >> l >> t;
+    int tsk = (n - 1) / 7 + 1;
+    cout << n - max((p - 1) / (2 * t + l) + 1, (p - t * tsk - 1) / l + 1) << endl;
+}   
+
+signed main() {
+    IO;
+    int T, TT = 1;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve(TT++);
+    }
+    return 0;
+}
