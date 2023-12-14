d81c7440dcb101438b46ebad93e0d881a566a3d2
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-12-14 18:46:55 +0300

    Create B - Две бинарных строки.cpp

---
+++ B - Две бинарных строки.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define VI vector<int>
+
+const int inf = 0x3f3f3f3f;
+const long long llinf = (long long)0x3f3f3f3f3f3f3f3f;
+const long long MOD = (long long)1e9 + 7LL;
+const size_t N = (size_t)1e6 + 5;
+
+#define IO                       \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);             \
+    std::cout.tie(0)
+
+#define debug(a) std::cerr << "\033[34m" << #a << ':' << a << "\033[0m" << std::endl
+
+#define debugList(list)                        \
+    std::cerr << "\033[34m" << #list << ": ["; \
+    for (auto& e : list) {                     \
+        std::cerr << e << ", ";                \
+    }                                          \
+    std::cerr << "\b\b]\033[0m" << endl
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    int ok = 0;
+    for (int i = 1; i < a.size(); i++) {
+        if (a[i - 1] == '0' && b[i - 1] == '0' && a[i] == '1' && b[i] == '1') ok = 1;
+    }
+    if (ok) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+}   
+
+signed main() {
+    IO;
+    int T;
+    T = 1; 
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
